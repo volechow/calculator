7cec01ce129e06e8f0d48271fcab45c748feb4b0
Victor Olechow <v.olechow@outlook.de>
2017-08-31 12:20:00 +0200

    Added decimal input

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
+function updateDisplay(ent, mem) {
+  entry = ent;
+  memory = mem;
+  $("#entry").text(ent);
+  $("#memory").text(mem);
+}
+
+var memory = "0";
+var entry = "0";
+
 $(document).ready(function() {
-  var memory = 0;
+  var cur = "";
+  var chained = false;
 
   $(".num").click(function() {
-    var mem = $("#entry").text();
-    var cur = $(this).text();
-    if (mem == "0") {
-      $("#entry").text(cur);
+    cur = $(this).text();
+    if (entry == "0") {
+      updateDisplay(cur, cur);
     } else {
-      $("#entry").text(mem+cur);
+      updateDisplay(entry+cur, memory+cur);
+    }
+    if (chained) {
+      updateDisplay(cur, memory+cur);
+      chained = false;
     }
   });
 
+  $("#decimal").click(function() {
+    cur = $(this).text();
+    updateDisplay(entry+cur, memory+cur);
+  });
+
+  $(".operator").click(function() {
+    cur = $(this).text();
+    updateDisplay(cur, memory+cur);
+    chained = true;
+  });
+
   $(".clear").click(function() {
-    $("#entry").text("0");
+    updateDisplay("0", "0");
   })
 
 });
