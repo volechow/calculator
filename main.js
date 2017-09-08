a93c4f3f451b32e5cedcd1404b63dfe4a6c296eb
Victor Olechow <v.olechow@outlook.de>
2017-09-08 19:39:08 +0200

    Some changes
    Changed how a valid number is identified.
    Changed layout design.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-function updateDisplay(entry, memory=null) {
-  $("#entry").text(entry);
-  if (memory != null) {
-    $("#memory").text(memory);
-  }
+
+/**
+* Updates Display accordingly.
+* @param {String} entry - current value string
+* @param {Number} memory - value in memory
+*/
+function updateDisplay(display, output) {
+  $("#"+display).text(output);
 }
 
+/**
+* Calculates value according to operator.
+* @param {Number} memory - value in memory
+* @param {Number} value - current value
+* @param {String} operator - mathematical operator string
+* @return {Number} calculated value from memory and current value
+*/
 function calculate(memory, value, operator) {
   switch (operator) {
     case "+":
@@ -17,58 +27,60 @@ function calculate(memory, value, operator) {
         return memory * value;
     default:
         return 0;
+  }
 }
 
+/**
+* Updates entry string if entry combined with current symbol is a valid input.
+* @param {String} entry - current value string
+* @param {String} sym - current entry symbol
+* @return {String} concatenated entry string 
+*/
+function checkNumber(input) {
+  var re = new RegExp(/^([+-])?([1-9][0-9]*)(\.[0-9]*)?$/);
+  return re.test(input);
 }
 
 $(document).ready(function() {
   var memory = 0;
-  var operator = "";
-  var start = false;
-
-  $(".num").click(function() {
-    var entry = $("#entry").text();
-    var cur = $(this).text();
-    if (entry == "0" || start) {
-      entry = cur;
-      start = false;
-    } else {
-      entry += cur;
-    }
-    updateDisplay(entry);
-  });
-
-  $("#decimal").click(function() {
-    var entry = $("#entry").text();
-    entry += $(this).text();
-    updateDisplay(entry);
-  });
+  var operator = "+";
 
   $(".operator").click(function() {
     var entry = $("#entry").text();
-    if (operator != "") {
+    if (checkNumber(entry)) {
       memory = calculate(memory, parseFloat(entry), operator);
-    } else {
-      memory = parseFloat(entry);
     }
     operator = $(this).text();
-    updateDisplay(0, memory);
   });
 
-  $("#eq").click(function() {
+  $(".num, #decimal, .operator, #eq").click(function() {
+    var mem = $("#memory").text();
     var entry = $("#entry").text();
-    if (operator != "") {
-      var result = calculate(memory, parseFloat(entry), operator);
+    var sym = $(this).text();
+    if (checkNumber(entry+sym)) {
+      updateDisplay("entry", entry+sym);
+    } else {
+      updateDisplay("entry", sym);
     }
-    operator = "";
-    start = true;
-    updateDisplay(result, 0);
   });
+
+  // $("#eq").click(function() {
+  //   var entry = $("#entry").text();
+  //   if (chained) {
+  //     memory = calculate(memory, parseFloat(entry), operator);
+  //     chained = false
+  //   } else {
+  //     memory = entry
+  //   }
+  //   operator = "";
+  //   updateEntry(memory);
+  //   updateMemory(0);
+  // });
 
   $(".clear").click(function() {
     memory = 0;
     operator = "";
-    updateDisplay(memory, memory);
-  })
+    updateEntry(0);
+  });
 
 });
